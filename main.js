e2c714d2b1120be330e63823b72a9759aac34a8c
Joel Pereyra <joelpereyra1993@gmail.com>
2024-03-11 01:06:50 +0100

    added cors origin

--- main.js
+++ main.js
@@ -3,59 +3,60 @@ const { MongoClient } = require("mongodb");
 const app = express();
 
 // Use CORS middleware
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 const port = process.env.PORT || 3000; //Radiofrekvens
 // const migrationHelper = require("./migrationhelper");
 
-app.use(cors({
-  origin: 'https://wall-of-text.netlify.app'
-}));
+app.use(
+  cors({
+    origin: "https://wall-of-text.netlify.app",
+    credentials: true,
+  })
+);
 
 const uri =
-"mongodb+srv://jpempire1777:Jolli9393!@cluster0.ijoagk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://jpempire1777:Jolli9393!@cluster0.ijoagk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri);
 app.use(express.json());
 //GET
 app.get("/walloftexts", async (req, res) => {
-    try {
-      await client.connect();
-      const database = client.db("walloftext");
-      const collection = database.collection("walloftexts");
-      const walloftexts = await collection.find({}).toArray();
-      res.json(walloftexts);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "Internal server error" });
-    } finally {
-      await client.close();
-    }
-  });
-  
-  //POST
-  app.post("/walloftexts", async (req, res) => {
-      try {
-        await client.connect();
-        const database = client.db("walloftext");
-        const collection = database.collection("walloftexts");
-        const result = await collection.insertOne(req.body);
-          // If the insertion was successful, send a 200 response
-          res.status(200).json({ message: "Message Successfully Added!" });
-       
-      } catch (err) {
-        // If an error occurred during the insertion process, send a 500 response
-        console.error(err);
-        res.status(500).json({ message: "Internal server error" });
-      } finally {
-        // Close the MongoDB client connection
-        await client.close();
-      }
-    });
-  
+  try {
+    await client.connect();
+    const database = client.db("walloftext");
+    const collection = database.collection("walloftexts");
+    const walloftexts = await collection.find({}).toArray();
+    res.json(walloftexts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  } finally {
+    await client.close();
+  }
+});
+
+//POST
+app.post("/walloftexts", async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db("walloftext");
+    const collection = database.collection("walloftexts");
+    const result = await collection.insertOne(req.body);
+    // If the insertion was successful, send a 200 response
+    res.status(200).json({ message: "Message Successfully Added!" });
+  } catch (err) {
+    // If an error occurred during the insertion process, send a 500 response
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  } finally {
+    // Close the MongoDB client connection
+    await client.close();
+  }
+});
 
 app.listen(port, async () => {
-    // await migrationHelper.migrate();
-    console.log(`Listening on port 👂🏽 ${port}`);
+  // await migrationHelper.migrate();
+  console.log(`Listening on port 👂🏽 ${port}`);
 });
