61f5f139b291379b05ee4d2c9340fb3670a0de53
Joel Pereyra <joelpereyra1993@gmail.com>
2024-03-11 01:13:22 +0100

    added cors origin

--- main.js
+++ main.js
@@ -4,59 +4,61 @@ const app = express();
 
 // Use CORS middleware
 const cors = require("cors");
-app.use(cors());
 
-const port = process.env.PORT || 3000; //Radiofrekvens
-// const migrationHelper = require("./migrationhelper");
+// Set up CORS options
+const corsOptions = {
+  origin: "https://wall-of-text.netlify.app",
+  credentials: true,
+};
 
-app.use(
-  cors({
-    origin: "https://wall-of-text.netlify.app",
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 
-const uri =
-  "mongodb+srv://jpempire1777:Jolli9393!@cluster0.ijoagk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const port = process.env.PORT || 3000;
 
-const client = new MongoClient(uri);
-app.use(express.json());
-//GET
-app.get("/walloftexts", async (req, res) => {
+const uri = "mongodb+srv://jpempire1777:Jolli9393!@cluster0.ijoagk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to MongoDB once
+async function connectToDatabase() {
   try {
     await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process if MongoDB connection fails
+  }
+}
+
+connectToDatabase();
+
+app.use(express.json());
+
+// GET route to fetch all messages
+app.get("/walloftexts", async (req, res) => {
+  try {
     const database = client.db("walloftext");
     const collection = database.collection("walloftexts");
     const walloftexts = await collection.find({}).toArray();
     res.json(walloftexts);
   } catch (err) {
-    console.error(err);
+    console.error("Error fetching messages:", err);
     res.status(500).json({ message: "Internal server error" });
-  } finally {
-    await client.close();
   }
 });
 
-//POST
+// POST route to add a new message
 app.post("/walloftexts", async (req, res) => {
   try {
-    await client.connect();
     const database = client.db("walloftext");
     const collection = database.collection("walloftexts");
     const result = await collection.insertOne(req.body);
-    // If the insertion was successful, send a 200 response
     res.status(200).json({ message: "Message Successfully Added!" });
   } catch (err) {
-    // If an error occurred during the insertion process, send a 500 response
-    console.error(err);
+    console.error("Error adding message:", err);
     res.status(500).json({ message: "Internal server error" });
-  } finally {
-    // Close the MongoDB client connection
-    await client.close();
   }
 });
 
-app.listen(port, async () => {
-  // await migrationHelper.migrate();
+app.listen(port, () => {
   console.log(`Listening on port 👂🏽 ${port}`);
 });
